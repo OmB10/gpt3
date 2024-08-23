60d3df4547281f430ff76b00d7e8663e01628c8c
OmB10 <ombamane10@gmail.com>
2024-08-23 19:16:36 +0530

    all the components and containers created

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
-    return (
-        <div>Feature</div>
-    )
-}
+const Feature = ({ title, text }) => (
+    <div className="features-container-feature">
+        <div className="features-container-feature-title">
+            <div />
+            <h1>{title}</h1>
+        </div>
+        <div className="features-container-feature-text">
+            <p>{text}</p>
+        </div>
+    </div>
+);
 
 export default Feature
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                 </div>
                 <div className='navbar-links-container'>
                     <p><a href="#home">Home</a></p>
-                    <p><a href="#Wgpt3">What is GPT?</a></p>
+                    <p><a href="#wgpt3">What is GPT?</a></p>
                     <p><a href="#possibility">Open AI</a></p>
                     <p><a href="#features">Case Studies</a></p>
                     <p><a href="#blog">Library</a></p>
